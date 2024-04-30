43440d9fff07be516752ca4adc8d11b6080215d3
zhengzhiyu <zhengzhiyuu@icloud.com>
2024-04-30 16:02:13 +0800

    动画，将某一个拖拽节点移动到指定的拖拽节点上，然后消失

--- assets/Scene.ts
+++ assets/Scene.ts
@@ -18,13 +18,59 @@ export default class Scene extends cc.Component implements IDragDropListener {
     // @property(cc.Node)
     // container: cc.Node = null;
 
+    @property(cc.Node)
+    containers: cc.Node = null;
+
+    containerItems: ContainerItem[] = [];
+
     start() {
+
+        this.containerItems = [];
+        for (let i = 0; i < this.containers.children.length; i++) {
+            const node = this.containers.children[i];
+            this.containerItems.push(node.getComponent(ContainerItem));
+        }
+
         DragDropMgr.getInstance().on(this, true, this.touchPanel);
         // DragDropMgr.getInstance().addContainer(this.container);
         // DragDropMgr.getInstance().registerDragTarget(this.drag, 0, (event) => {
         //     let node = event.currentTarget as Node;
         //     console.log(`DragDropScene-> click `, node);
         // });
+
+        this.dragMoveFromTo(0, 3);
+    }
+
+    convertPos(fromNode: cc.Node, toNode: cc.Node) {
+        let worldPosA = fromNode.convertToWorldSpaceAR(cc.v2());
+        let localPosB = toNode.convertToNodeSpaceAR(worldPosA);
+        return localPosB;
+    }
+
+    /**
+     * 动画，将某一个拖拽节点移动到指定的拖拽节点上，然后消失
+     * @param moveIdx 
+     * @param toIdx 
+     * @param callack 
+     */
+    dragMoveFromTo(moveIdx: number, toIdx: number, callack?: Function) {
+        let moveDragItem = this.containerItems[moveIdx].drag,
+            toDragItem = this.containerItems[toIdx].drag;
+        let tempNode = cc.instantiate(moveDragItem.node);
+        tempNode.getComponent(DragItem).enabled = false;
+        tempNode.parent = this.node;
+        moveDragItem.setIcon(null);
+        tempNode.setPosition(this.convertPos(moveDragItem.node, this.node));
+
+        let toPos = this.convertPos(toDragItem.node, this.node);
+        cc
+            .tween(tempNode)
+            .to(0.5, { position: cc.v3(toPos) }, { easing: 'quadIn' })
+            .call(() => {
+                tempNode.destroy();
+                callack && callack();
+            })
+            .start();
     }
 
     onCreateDragAgentData(touchTarget: cc.Node, uiLocation: cc.Vec2): any {
